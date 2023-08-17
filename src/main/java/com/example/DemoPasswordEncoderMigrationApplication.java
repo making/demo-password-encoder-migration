65f60da132dbe43b68a8eab12022144eb5fd46f2
Toshiaki Maki <makingx@gmail.com>
2023-08-17 10:53:54 +0900

    init

---
+++ src/main/java/com/example/DemoPasswordEncoderMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPasswordEncoderMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPasswordEncoderMigrationApplication.class, args);
+    }
+
+}
