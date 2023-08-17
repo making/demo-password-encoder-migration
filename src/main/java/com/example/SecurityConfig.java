23fdac22114e09229344a6c52ea588e7f0e0d5d1
Toshiaki Maki <makingx@gmail.com>
2023-08-17 14:52:54 +0900

    Use minimal DelegatingPasswordEncoder

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.DelegatingSecurityContextRepository;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+
+import java.util.Map;
 
 @Configuration
 public class SecurityConfig {
@@ -27,7 +30,10 @@ public class SecurityConfig {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		String idForEncode = "bcrypt";
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idForEncode,
+				Map.of(idForEncode, new BCryptPasswordEncoder()));
+		return passwordEncoder;
 	}
 
 	@Bean
